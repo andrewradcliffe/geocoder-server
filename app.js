0c4aeb0398ba6d55dfe70e0613dcaac5a3121f05
Andrew Radcliffe <andrew.radcliffe@navtor.com>
2024-10-03 10:56:59 +0100

    Add country code route and support for nodemon

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
 import express from 'express';
+import * as countryCoder from '@rapideditor/country-coder';
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.get('/getCountryCode', (req, res) => {
+    //get lat and long from request
+    const latitude = req.query.lat;
+    const longitude = req.query.long;
+
+    if (!latitude || !longitude) {
+        res.status(400).send({
+            code: 400,
+            error: 'Latitude and longitude are required'
+        });
+        return;
+    }
+
+    //get country code from lat and long
+    const countryCode = countryCoder.iso1A2Code([longitude, latitude]);
+
+    if (!countryCode) {
+        res.status(404).send({
+            code: 404,
+            error: 'Country code not found'
+        });
+        return;
+    }
+
+    const response = {
+        code: 200,
+        countryCode: countryCode
+    }
+
+    res.send(response);
+    return;
 });
 
 app.listen(port, () => {
